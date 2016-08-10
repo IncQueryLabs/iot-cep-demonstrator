d6066f60abd5cdb836d2f6efaa175138226f576e
pappist <istvan.papp@incquerylabs.com>
2016-08-10 15:40:32 +0200

    #3 Implemented leap motion frame recorder

---
+++ lm-demo/com.incquerylabs.iot.leapmotion/src/main/java/com/incquerylabs/iot/leapmotion/frame/FrameRecorder.java
@@ -0,0 +1,40 @@
+package com.incquerylabs.iot.leapmotion.frame;
+
+import java.io.DataOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import com.leapmotion.leap.Controller;
+import com.leapmotion.leap.Frame;
+import com.leapmotion.leap.Listener;
+
+public class FrameRecorder extends Listener {
+	
+	DataOutputStream outputStream;
+	
+	public FrameRecorder(String outputfilepath) throws IOException {
+		outputStream = new DataOutputStream(new FileOutputStream(outputfilepath));
+	}
+	
+	@Override
+	public void onFrame(Controller controller) {
+		
+		Frame frame = controller.frame();
+		
+		if(!frame.isValid() || frame.hands().isEmpty()) return;
+		
+		try {
+			outputStream.writeInt(frame.serializeLength());
+			outputStream.write(frame.serialize());
+			outputStream.flush();
+		} catch (IOException e) {
+			System.out.println(String.format("Unable to write frame (id / timestamp): %d / %d",frame.id(), frame.timestamp()));
+		}
+		
+	}
+	
+	public void dispose() throws IOException {
+		outputStream.close();
+	}
+	
+}
