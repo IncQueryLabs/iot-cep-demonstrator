efc388838798c0ed7a4f7eef5d000b57046ceb6a
pappist <istvan.papp@incquerylabs.com>
2016-09-15 22:10:57 +0200

    #7 Added web bridge project

---
+++ iot-commons/com.incquerylabs.iot.web-bridge/src/main/webapp/javascript/wsbridge.js
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+
+$(
+	function () {
+	
+		var input = $('#input');
+		var header = $('#header');
+		var status = $('#status');
+		var content = $('#content');
+		
+	    var socket = atmosphere;
+	    var subSocket;
+	    var transport = 'websocket';
+	    
+	    
+	    
+}
+		
+
+);
